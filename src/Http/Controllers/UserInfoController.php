750f5d0b8cb8f5f7f58192c84a764f9394756594
Cesar <cesargb@gmail.com>
2022-06-15 06:55:39 +0200

    refactor user info controller

--- src/Http/Controllers/UserInfoController.php
+++ src/Http/Controllers/UserInfoController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 
-class UserController extends Controller
+class UserInfoController extends Controller
 {
-    public function show(): JsonResponse
+    public function __invoke(): JsonResponse
     {
         return response()->json(Auth::user());
     }
