4a2dcad3c273ba2c479a9306efb6fcff2151d9a2
Cesar <cesargb@gmail.com>
2022-06-15 08:56:41 +0200

    refactor route definitions

--- routes/routes.php
+++ routes/routes.php
@@ -8,22 +8,25 @@ use Descom\AuthSpa\Http\Controllers\UserController;
 use Descom\AuthSpa\Http\Controllers\ProfileInfoController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('login', LoginController::class)
+    ->middleware(['web'])
+    ->name('login');
 
-Route::group(['middleware' => ['web']], function () {
-    Route::post('login', LoginController::class)->name('login');
-    Route::post('logout', LogoutController::class)->name('logout');
-    Route::post('/password/reset_link', ResetLinkController::class)->name('password.reset_link');
-    Route::post('/password/reset', ResetController::class)->name('password.reset');
-});
+Route::post('logout', LogoutController::class)
+    ->middleware(['web'])
+    ->name('logout');
 
-Route::group(['middleware' => ['api', 'auth:sanctum']], function () {
-    Route::get('user', config('auth_spa.controllers.user_info',  ProfileInfoController::class))
-        ->name('profile.info');
-});
+Route::post('/password/reset_link', ResetLinkController::class)
+    ->middleware(['web'])
+    ->name('password.reset_link');
 
+Route::post('/password/reset', ResetController::class)
+    ->middleware(['web'])
+    ->name('password.reset');
 
 Route::get(
     config('auth_spa.http.profile_info.path',  'user'),
     config('auth_spa.http.profile_info.controller',  ProfileInfoController::class)
-)->middleware(config('auth_spa.http.profile_info.middleware', ['api', 'auth:sanctum']))
-->name('profile.info');
+)
+    ->middleware(config('auth_spa.http.profile_info.middleware', ['api', 'auth:sanctum']))
+    ->name('profile.info');
